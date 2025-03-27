39f0196571c9c4836debc036b07c6498684a7a1e
Ayan <ayanbar2017@gmail.com>
2025-03-27 19:57:39 +0200

    multiplanet system

--- solarsystem.py
+++ solarsystem.py
@@ -4,71 +4,69 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 G = 6.67430e-11 #constanta gravitationala 
+# am decis sa il fac dictionar
 
-class Body:#poate trebuie densitatea?
-    def __init__(self,mass,position,velocity,color,size):
-        self.mass = mass
-        self.position = np.array(position)
-        self.velocity = np.array(velocity)
-        self.color = color
-        self.size = size
+def create_body (name,mass,position, velocity,color,size):
+    return{
+        "name": name,
+        "mass": mass,
+        "position": np.array(position, dtype='float64'),
+        "velocity": np.array(velocity, dtype='float64'),
+        "color": color,
+        "size": size,
+    }
+#pozitia mai usor de accesat sa fac calcule
+def update_position(body, dt):
+    body["position"] += body["velocity"] * dt
 
-    def update_position(self,dt):
-        self.position = self.position + self.velocity * dt
-    
-    def apply_gravitational_force(self,other):
-        vector = other.position - self.position
-        distance = np.linalg.norm(vector)
+def apply_gravitational_force(body1,body2):
 
-        if(distance == 0):# failsafe
-            return np.array([0,0])
-        force_magnitude = G * self.mass * other.mass / distance**2
-        force_direction = vector/distance
+    vector = body2["position"] - body1["position"]
+    distance = np.linalg.norm(vector)#metoda fancy si "eficienta" sa calculezi sqrt(x^2+y^2)
+    if distance == 0:#failsafe daca distanta e 0
+        return np.array([0.0, 0.0])
+    force_magnitude = G * body1["mass"] * body2["mass"] / distance**2
+    force_direction = vector / distance
+    return force_magnitude * force_direction
 
-        force = force_magnitude * force_direction
+def update_velocity(body,force,dt):
+    acceleration = force/body["mass"]
+    body["velocity"] += acceleration * dt
 
-        return force
-    
-    def update_velocity(self,force,dt):
-        acceleration = force/self.mass
-        self.velocity = self.velocity + acceleration * dt 
-
-
-
-sun = Body(1.989e30,[0,0],[0,0],'yellow',15)
-mars = Body(6.417e23,[227.9e9,0],[0,24.077e3],'red',5)
-earth = Body(5.972e24, [1.496e11, 0], [0, 29.78e3], 'green', 5)
-
+bodies = [
+    create_body("Sun",1.989e30, [0, 0], [0, 0], "yellow", 15),  #Sun
+    create_body("Earth",5.972e24, [1.496e11, 0], [0, 29.78e3], "blue", 5),  #Earth
+    create_body("Mars",6.417e23, [227.9e9, 0], [0, 24.077e3], "red", 5),  #Mars
+    create_body("Venus",4.867e24, [1.082e11, 0], [0, 35.02e3], "orange", 5),  #Venus
+    create_body("Mercury",3.285e23, [57.91e9, 0], [0, 47.87e3], "gray", 4),  # fuger mercur
+]
 #creaza ploturile
 fig, ax = plt.subplots()
 #limitle de x,y
 ax.set_xlim(-3e11,3e11)
 ax.set_ylim(-3e11,3e11)
 
-dt = 60*60 *24# 1 zi in secunde
-sun_plot = ax.scatter(sun.position[0], sun.position[1], color = "yellow", s=sun.size)
-mars_plot = ax.scatter(mars.position[0], mars.position[1], color = "red", s=mars.size)
-earth_plot = ax.scatter(earth.position[0], earth.position[1], color=earth.color, s=earth.size)
+plots = [ax.scatter(body["position"][0], body["position"][1], color=body["color"], s=body["size"]) for body in bodies]#crazy in line coding 👌
 
-bodies = [sun,mars,earth]
+dt = 60*60 *12# 1 zi in secunde
+
+
+
 
 def update(frame):
     for i, body1 in enumerate(bodies):
         total_force = np.array([0.0, 0.0])
         for j, body2 in enumerate(bodies):
             if i != j:  
-                force = body1.apply_gravitational_force(body2)
-                total_force += force 
+                total_force = total_force + apply_gravitational_force(body1, body2)
+            update_velocity(body1, total_force, dt)
+            update_position(body1, dt)
         
-        body1.update_velocity(total_force, dt)
-        body1.update_position(dt)
-    
-    sun_plot.set_offsets(sun.position)
-    mars_plot.set_offsets(mars.position)
-    earth_plot.set_offsets(earth.position)
+    for i,plot in enumerate(plots):
+        plot.set_offsets(bodies[i]["position"])
 
-    return sun_plot, mars_plot, earth_plot
+    return plots
 
-ani = FuncAnimation(fig, update, frames=365, interval=5, blit=True)
+ani = FuncAnimation(fig, update, frames=365, interval=20, blit=True)
 
 plt.show()
